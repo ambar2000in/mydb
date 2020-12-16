a8360703d7f2823fc5362d74c92a3c388cf2cc55
ambar2000in <72028844+ambar2000in@users.noreply.github.com>
2020-12-16 05:59:52 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+function myFunction1(e)
+{      
+      keynum=e.keyCode;
+      if (keynum==13)
+      {
+      document.getElementById("name").focus();
+      return false;                       //Cancel the Event of carriage return
+      }
+}
+
+
+function myFunction2(e)
+      {      
+            keynum=e.keyCode;
+            if (keynum==13)
+            {
+            document.getElementById("age").focus();
+            return false;                       //Cancel the Event of carriage return
+            }
+      }
+      
+
+function myFunction3(e)
+      {      
+            keynum=e.keyCode;
+            if (keynum==13)
+            {
+            document.getElementById("gender").focus();
+            return false;                       //Cancel the Event of carriage return
+            }
+      }
+
+function myFunction4(e)
+      {      
+            keynum=e.keyCode;
+            if (keynum==13)
+            {
+            document.getElementById("email").focus();
+            return false;                       //Cancel the Event of carriage return
+            }
+      }
+
+function myFunction5(e)
+      {      
+            keynum=e.keyCode;
+            if (keynum==13)
+            {
+            document.getElementById("mdesc").focus();
+            return false;                       //Cancel the Event of carriage return
+            }
+      }      
+
+
+
+      
+      function myFunction6(e)
+      {      
+            keynum=e.keyCode;
+            if (keynum==13 && keynum==ctrlKey)
+            {
+            document.getElementById("mdesc").focus();
+            return false;                       //Cancel the Event of carriage return
+            }
+      }      
--- myindex.php
+++ myindex.php
@@ -3,30 +3,30 @@
 
 if (isset($_POST['name']))
 {
-/*$server = "localhost";
-$username = "root";
-$password = "";
-$db ="mydb";*/
+//$server = "localhost";
+//$username = "root";
+//$password = "";
+//$db ="mydb";
 
            
- // PORT 3306 
+   // PORT 3306 
            
 $server = "bm2r5yopsfrjtyafew6x-mysql.services.clever-cloud.com:3306";
 $username = "ulmn8jagnfx4kcpx";
- $password="FqeR2bzSHuGSoRWizQKV";
+$password="FqeR2bzSHuGSoRWizQKV";
 $db ="bm2r5yopsfrjtyafew6x";
 
 $con = mysqli_connect($server, $username, $password, $db);
 
 if(!$con)
 {
+    
     die("Connection to this database failed due to ". mysqli_connect_error());
+    
 }    
  
 
-echo '<script>alert("Successfully connection");
- window.location = "index.html";
- </script>'; 
+echo ("Successfully connection");
 
 
 $sno=$_POST['sno'];
@@ -37,10 +37,7 @@ $email=$_POST['email'];
 /*$phone=$_POST['phone'];*/
 $mdesc=$_POST['mdesc'];
  
-/*$sql="INSERT INTO trip (sno, name, age, gender, email,  mdesc, dt)
-values ('$sno', '$name', '$age', '$gender', '$email', '$mdesc', current_timestamp());";*/
- 
- /*Serial number Automatically Increaments*/
+//Serial number Automatically Increaments
  
  $sql="INSERT INTO trip (name, age, gender, email,  mdesc, dt)
 values ('$name', '$age', '$gender', '$email', '$mdesc', current_timestamp());";
@@ -49,7 +46,8 @@ values ('$name', '$age', '$gender', '$email', '$mdesc', current_timestamp());";
 if ($con->query($sql) == true)
 {
     
-    echo '<script>alert("Successfully Inserted")</script>'; 
+    echo ("Successfully Inserted"); 
+    
          
     $insert=true;
 }
@@ -67,4 +65,5 @@ $con->close();
 
 
 header("location:index.html");   //REDIRECT TO CALLING FILE
+
 ?>
